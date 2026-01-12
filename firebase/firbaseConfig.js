df5f915d15dba31b9798924e56fd2a7bc3f6376a
forgetzz <forget.noxa90@gmail.com>
2026-01-13 03:10:00 +0800

    new

--- firebase/firbaseConfig.js
+++ firebase/firbaseConfig.js
@@ -1,10 +1,25 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("../google-services.json");
+const { initializeApp, cert } = require("firebase-admin/app");
+const { getFirestore } = require("firebase-admin/firestore");
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+const serviceAccountString = process.env.FIREBASE_SERVICE_ACCOUNT;
 
-const db = admin.firestore();
+if (!serviceAccountString) {
+  throw new Error("❌ ENV FIREBASE_SERVICE_ACCOUNT tidak ditemukan!");
+}
 
-module.exports = { db , admin};
+let serviceAccount = JSON.parse(serviceAccountString);
+
+// perbaiki private_key agar formatnya benar
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, "\n");
+
+// Inisialisasi Firebase Admin
+if (!admin.apps.length) {
+  initializeApp({
+    credential: cert(serviceAccount),
+  });
+}
+
+const db = getFirestore();
+
+module.exports = { admin, db };
