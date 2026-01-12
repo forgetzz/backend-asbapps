e7f5c4fbd00ffab86b8312b2bb877b0077168bac
forgetzz <128070357+forgetzz@users.noreply.github.com>
2026-01-13 03:06:51 +0800

    Update firbaseConfig.js

--- firebase/firbaseConfig.js
+++ firebase/firbaseConfig.js
@@ -1,5 +1,5 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("../google-services.json");
+const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT;
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
